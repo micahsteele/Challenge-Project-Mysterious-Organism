413582b95e33e0e9c141a387354473f14327ad82
Micah Steele <micahs66@gmail.com>
2024-09-19 16:34:28 +1000

    obj func returns mirror of initial array

--- js.js
+++ js.js
@@ -12,54 +12,68 @@ const returnRandBase = () => {
     }
     return newStrand
   }
-  let num = Math.floor(Math.random() * 10000)
   
-  function pAequorFactory(num, func) {
+function pAequorFactory(num, func) {
     return {
-      specimenNum: num,
-      dna: mockUpStrand(),
-      mutate() {
-        let spot = Math.floor(Math.random() * this.dna.length)
-        console.log('location: ' + spot)
-        let index = this.dna[spot]
-        let base = index
-        console.log(`index: ` + index)
-        while(index == base) {
-              base = returnRandBase()
-           }
-        console.log(`base: `+ base)
-        return this.dna[spot] =  base
-      },
-      compareDNA(otherDNA) {
-        let shared = 0;
-        let sharedDNA = [];
-        for(let i = 0; i < this.dna.length; i++) {
-            if(this.dna[i] === otherDNA.dna[i]) {
-              shared++
-              sharedDNA.push(this.dna[i])
+        specimenNum: num,
+        dna: mockUpStrand(),
+        mutate() {
+            let spot = Math.floor(Math.random() * this.dna.length)
+            console.log('location: ' + spot)
+            let index = this.dna[spot]
+            let base = index
+            console.log(`index: ` + index)
+            while(index == base) {
+                base = returnRandBase()
+            }
+            console.log(`base: `+ base)
+            return this.dna[spot] =  base
+        },
+        compareDNA(otherDNA) {
+            let shared = 0;
+            let sharedDNA = [];
+            for(let i = 0; i < this.dna.length; i++) {
+                if(this.dna[i] === otherDNA.dna[i]) {
+                shared++
+                sharedDNA.push(this.dna[i])
+                };
             };
-          };
-        console.log(sharedDNA);
-        let percentageShared = (shared / this.dna.length) * 100;
-        console.log(`specimen #1 and specimen #2 have ${percentageShared.toFixed(1)}% DNA in common`);
-      },
-    willLikelySurvive() {
-      let numberOfTimes = 0
-      for(let i = 0; i < this.dna.length; i++) {
-        if(this.dna[i] == 'C') {
-          numberOfTimes++
-        } else if (this.dna[i] == 'G') {
-          numberOfTimes++
+            console.log(sharedDNA);
+            let percentageShared = (shared / this.dna.length) * 100;
+            console.log(`specimen #1 and specimen #2 have ${percentageShared.toFixed(1)}% DNA in common`);
+        },
+        willLikelySurvive() {
+        let numberOfTimes = 0
+        for(let i = 0; i < this.dna.length; i++) {
+            if(this.dna[i] == 'C') {
+            numberOfTimes++
+            } else if (this.dna[i] == 'G') {
+            numberOfTimes++
+            }
+            }
+        if(numberOfTimes >= 9) {
+            return true
+        } else {
+            return false
         }
-        }
-      if(numberOfTimes >= 9) {
-        return true
-      } else {
-        return false
-      }
-      }
+        },
+        complementStrand() {
+            let complementaryStrand = [];
+            for(let i = 0; i < this.dna.length; i++) {
+              if (this.dna[i] == 'C') {
+                complementaryStrand.push('G')
+              } else if (this.dna[i] == 'G') {
+                complementaryStrand.push('C')
+              } else if (this.dna[i] == 'A') {
+                complementaryStrand.push('T')
+              } else if (this.dna[i] == 'T') {
+                complementaryStrand.push('A')
+              }
+            }
+            return complementaryStrand;
+          }
     }
-  };
+};
   
   const test = pAequorFactory(1, mockUpStrand());
   const test2 = pAequorFactory(2, mockUpStrand());
@@ -80,5 +94,7 @@ const returnRandBase = () => {
       }
     }
     return testRequests
-  }
-  console.log(createTestRequests(30))
+  };
+  console.log(createTestRequests(30));
+  console.log(test.complementStrand());
+
